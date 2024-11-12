a6de311b9d76c04d45bce6bf120ae550f192c2c0
Anand19987i <anandpandey1765@gmail.com>
2024-11-12 10:06:37 +0530

    Updates

--- backend/config/tokenUtils.js
+++ backend/config/tokenUtils.js
@@ -4,7 +4,7 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 const COOKIE_OPTIONS = {
     httpOnly: true,  // Cookie will not be accessible via JavaScript
-    secure: true,    // Set to true in production with HTTPS
+    secure: process.env.NODE_ENV === "production",    // Set to true in production with HTTPS
     sameSite: 'strict',  // Ensures cookie is only sent in first-party context
     maxAge: 7 * 24 * 60 * 60 * 1000,  // 1 week
 };
--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,8 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import cors from "cors";
-import userRoute from "./routes/user.route.js"
-import postRoute from "./routes/post.route.js"
+import userRoute from "./routes/user.route.js";
+import postRoute from "./routes/post.route.js";
 import isAuthenticated from "./middlewares/isAuthenticated.js";
 
 dotenv.config({});
@@ -14,25 +14,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const corsOptipn = {
+const corsOptions = {
     origin: 'https://blurb-6zkl.onrender.com', // replace with your frontend domain
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // allowed methods
-    credentials: true // enable cookies if needed
-}
+    credentials: true, // allow cookies if needed
+};
 
+app.use(cors(corsOptions));
 app.use((req, res, next) => {
+    console.log('Setting CORS headers');
     res.header('Access-Control-Allow-Origin', 'https://blurb-6zkl.onrender.com');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
     next();
 });
-app.use(cors(corsOptipn));
+
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/post", postRoute);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
     connectDB();
-    console.log(`Server is listening at ${port}`)
-})
+    console.log(`Server is listening at ${port}`);
+});
