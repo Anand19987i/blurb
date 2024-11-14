18e68d78c0ae6c37a7d0e25a4d02e651f4a9738f
Anand19987i <anandpandey1765@gmail.com>
2024-11-14 21:53:46 +0530

    Trying

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,3 +1,3 @@
-export const USER_API_END_POINT="http://localhost:3000/api/v1/user";
-export const POST_API_END_POINT="http://localhost:3000/api/v1/post";
+export const USER_API_END_POINT="https://blurbb.onrender.com/api/v1/user";
+export const POST_API_END_POINT="https://blurbb.onrender.com/api/v1/post";
 // http://localhost:3000
